9a16a4ac783201e7a5b0f30180d933166ed0fdec
kareenb <kareenb89@gmail.com>
2018-07-25 16:55:58 +0300

    t

--- src/router.js
+++ src/router.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from './views/home.vue'
 import about from './views/about.vue'
-<<<<<<< HEAD
 import issueDetails from './views/issueDetails.vue'
-=======
 import addIssue from './views/issueAdd.vue'
->>>>>>> 8833c63f5596af8a2fcd01ef4e08486a462da9e4
 
 Vue.use(Router)
 
@@ -22,15 +19,14 @@ export default new Router({
       component: about
     },
     {
-<<<<<<< HEAD
       path: '/issueDetails/:issueId?',
       name: 'issueDetails',
       component: issueDetails
-=======
+    },
+    {
       path: '/issue/add',
       name: 'add issue',
       component: addIssue
->>>>>>> 8833c63f5596af8a2fcd01ef4e08486a462da9e4
     }
   ]
 })
