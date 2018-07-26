a5bf63d2e2a06373605998c7c8c5bcfe25954477
markop85 <mark.kopolov@gmail.com>
2018-07-26 15:47:05 +0300

    mark push

--- src/store/commentModule.js
+++ src/store/commentModule.js
@@ -1,19 +1,51 @@
 'use strict';
 
+import commentService from '../services/commentService.js';
+
+export const GET_COMMENTS = 'comments/action/getComments'
+export const ADD_COMMENT = 'comment/action/addComment'
+export const SET_COMMENTS = 'comments/mutations/setComments'
+export const SET_COMMENT = 'comment/mutations/setComment'
+export const LOAD_COMMENTS = 'comment/getters/loadComments'
 export default {
     state: {
         comments: []
     },
 
     mutations: {
-
+        [SET_COMMENTS](state, {comments}) {
+            state.comments = comments;
+        },
+        [SET_COMMENT](state, {comment}) {
+            state.comments.push(comment);
+        },
     },
 
     getters: {
-        
+        [LOAD_COMMENTS](state) {
+            return state.comments;
+        }
     },
 
     actions: {
+        [GET_COMMENTS](context, {issueId}) {
+            return commentService.getComments(issueId)
+                .then(comments => {
+                    context.commit({ type: SET_COMMENTS, comments });
+                    return comments
+                })
+                .catch((err) => {console.warn(err)
+                });
+        },
+
+        [ADD_COMMENT](context, {comment}) {
+            return commentService.addComment(comment)
+                .then(comment => {
+                    context.commit({ type: SET_COMMENT, comment });
+                    return comment;
+                }).catch(err=> console.warn(err))    
+        }    
+            
 
     }
 }
--- src/views/issueDetails.vue
+++ src/views/issueDetails.vue
@@ -1,62 +1,112 @@
 
 <template>
-    <section v-if = 'issue'>
-      <GmapMap
-  :center="issue.loc"
-  :zoom="12"
-  map-type-id="terrain"
-  style="width: 500px; height: 300px"
->
-  <GmapMarker
-    :position="issue.loc"
-  />
-</GmapMap>
+    <section v-if = "issue && comments" class="details-container flex column" >
+      <GmapMap v-if = "issue.loc" :center="issue.loc" :zoom="12" map-type-id="terrain" style="min-width: 500px; min-height: 200px">
+        <GmapMarker :position="issue.loc"/>
+      </GmapMap>
 
-      <div>{{issue.title}}</div>
-      <div>{{issue.desc}}</div>
-      <div>{{issue.category}}</div>
-      <div>{{issue.status}}</div>
+      <h1>{{issue.title}}</h1>
+      <div class="issue-container flex column">
+      <div>Description: {{issue.desc}}</div>
+      <div>Category: {{issue.category}}</div>
+      <div>Status: {{issue.status}}</div>
+      </div>
 
-      <carousel :per-page="1">
-  <slide>
-      <img src="https://i.pinimg.com/originals/2a/0a/91/2a0a91b9a6e1db1e717963baf73aa9ed.png" >
-  </slide>
-  <slide>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQMPhXXBzALNe-u0ev7fFv-iLAWJ3GcdcKzBOB9nJgM9Q4sIKESA" alt="">
-  </slide>
-  <slide>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQMPhXXBzALNe-u0ev7fFv-iLAWJ3GcdcKzBOB9nJgM9Q4sIKESA" alt="">
-  </slide>
-  <slide>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQMPhXXBzALNe-u0ev7fFv-iLAWJ3GcdcKzBOB9nJgM9Q4sIKESA" alt="">
-  </slide>
-</carousel>
+
+      <carousel :per-page="1" >
+        <slide v-for="(img,idx) in issue.imgUrls" :key="idx">
+          <img :src="img" >
+        </slide>
+        
+      </carousel>
+
+
+            <h2>Comments:</h2><button class="add-btn" @click = "toggleModal">Add Comment</button>
+        <div class="comments-container" v-for ='comment in comments' :key="comment._id">
+            <div class="card-container">
+            <img :src = 'comment.commenter.imgUrl'><div class="comment-box">{{comment.txt}}</div>
+            </div>
+        </div>
+        
+            <div v-if="openModal" class="reply-box flex" >
+              <div class="reply"  >comment:
+              <button @click="toggleModal">X</button>
+                <div contenteditable="true" ref="commentContent" v-focus = true></div>
+                <button  @click="addComment">Add Comment</button>
+              </div>
+            </div>
+
+
     </section>
 </template>
 
 <script>
 import { GET_ISSUE_BY_ID } from '@/store/issueModule.js';
+import { GET_COMMENTS } from '@/store/commentModule.js';
+import { GET_USER } from '@/store/userModule.js';
+import { ADD_COMMENT } from '@/store/commentModule.js';
 import { Carousel, Slide } from 'vue-carousel';
-
-// import issueListCmp from '@/components/issueCmps/issueListCmp.vue';
 
 export default {
 
   data() {
     return {
-      issue: null,
-    };
-  },
+      issue: {},
+      comments:[],
+      openModal :false,
+    }
+  },  
+
+    computed:{
+      
+    },
+  
 
   created() {
-    let issueId = this.$route.params.issueId;
-    this.$store.dispatch({ type: GET_ISSUE_BY_ID, issueId })
-      .then(issue => {
-        this.issue = issue;
-      }).catch(err => console.warn(err))
+      let issueId = this.$route.params.issueId;
+      this.getIssue(issueId);
+      this.getComments(issueId);
+    },
+
+
+  methods: {
+    getIssue(issueId) {
+      this.$store.dispatch({ type: GET_ISSUE_BY_ID, issueId })
+        .then(issue => {
+          this.issue = issue;
+        }).catch(err => console.warn(err))
+    },
+
+    getComments(issueId) {
+      this.$store.dispatch({ type: GET_COMMENTS, issueId })
+        .then(comments=> {
+          this.comments = comments;
+        }).catch(err => console.warn(err))
+    },
+
+    addComment() {
+      var txt = this.$refs.commentContent.innerText;
+      var user = this.$store.getters[GET_USER];
+      this.$store.dispatch({type: ADD_COMMENT, comment:{issueId: this.issue._id, commenterId: user._id,
+      txt, createdAt: Date.now()}})
+        .then(comment=> console.log('comment added'))
+        .catch(err=>console.warn(err))
+        this.toggleModal();
+    },
+
+    toggleModal() {
+      this.openModal = (this.openModal)? false : true;
+      if(this.openModal) this.$refs.commentContent.focus();
+    }
   },
 
-  computed: {},
+  directives: {
+  focus: {
+    inserted: function (el) {
+      el.focus()
+    }
+  }
+},
 
     components: {
       Carousel,
@@ -66,3 +116,57 @@ export default {
     }
 };
 </script>
+
+<style lang="scss" scoped>
+  .details-container  {
+    position: relative;
+    width: 700px;
+    margin: 0 auto;
+    outline: 1px solid gray;
+    height: 100%;
+    .issue-container {
+      float: left;
+    }
+      .add-btn{
+        padding: 30px;
+        width: 100px;
+        margin: 0 auto;   
+        margin-bottom: 10px;     
+      }
+    .comments-container {
+      .comment-box {
+        margin: 0 auto;
+        width: 300px;
+        height: auto;
+        background: #E5E3DF;
+        box-shadow: 10px 10px 9px -1px rgba(0,0,0,0.64);
+      }
+      .card-container {
+        margin-bottom: 10px;
+          img {
+            width: 20px;
+            height: 20px;
+          }
+      }
+
+    }
+        .reply-box {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          justify-content: center;
+          align-items: center; 
+          background-color: rgba(237, 237, 245, 0.596);
+        }
+
+        .reply {
+          width:300px;
+          height: 200px;
+          background: grey;
+          opacity: 1;
+
+        }
+          
+  }
+</style>
+
